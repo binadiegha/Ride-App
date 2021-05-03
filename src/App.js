c74b075762173d298843800437fea394171bdeff
binadiegha <jonesbgabriel@gmail.com>
2021-05-03 21:58:08 +0100

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Home from "./components/home";
 import Car from "./components/car";
 import './App.less';
@@ -10,29 +10,36 @@ import Errands from './components/errands';
 import RideMode from './components/ride-mode';
 import WelcomeMusic from './components/welcome-music';
 
+
 function App() {
+
+ 
+
   return (
-    <div className="holder">
 
-<div className="rotate">
-     {/* <Home /> */}
-     <Router>
+      <div className="holder">
 
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/car/:id" component={Car}  />
-        <Route path="/car-music" component={CarMusic}  />
-        <Route path="/driver/:id" component={Driver}  />
-        <Route path="/nameplate/:id" component={NamePlate}  />
-        <Route path="/errands/:id" component={Errands}  />
-        <Route path="/ride-mode/:id" component={RideMode}  />
-        <Route path="/welcome-music/:id" component={WelcomeMusic}  />
-      </Switch>
-    </Router>
+      <div className="rotate">
+      {/* <Home /> */}
+      <Router>
 
-    </div>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/car/:id" component={Car}  />
+          <Route path="/car-music" component={CarMusic}  />
+          <Route path="/driver/:id" component={Driver}  />
+          <Route path="/nameplate/:id" component={NamePlate}  />
+          <Route path="/errands/:id" component={Errands}  />
+          <Route path="/ride-mode/:id" component={RideMode}  />
+          <Route path="/welcome-music/:id" component={WelcomeMusic}  />
+        </Switch>
+      </Router>
 
-    </div>
+      </div>
+
+      </div>
+
+    
     
   );
 }
